7c77934a5a917a96f9f89b287e3df5e554417f2a
alagunag29 <alagunag29@gmail.com>
2018-12-19 20:18:20 -0500

    Updates

---
+++ precache-manifest.0bed4f64b8818a7bf25cb635867e65a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a9e12f02c7847cc30a3156c3c7c04ae",
+    "url": "/repo/static/media/send-button.0a9e12f0.svg"
+  },
+  {
+    "revision": "2d437b64d9f02ad75856551af2ef04dd",
+    "url": "/repo/static/media/ok-mark.2d437b64.svg"
+  },
+  {
+    "revision": "3da021c15adfa4073df2",
+    "url": "/repo/static/js/runtime~main.3da021c1.js"
+  },
+  {
+    "revision": "17022e99cf80a3ca1474",
+    "url": "/repo/static/js/main.17022e99.chunk.js"
+  },
+  {
+    "revision": "b4b34526f0845d145751",
+    "url": "/repo/static/js/1.b4b34526.chunk.js"
+  },
+  {
+    "revision": "17022e99cf80a3ca1474",
+    "url": "/repo/static/css/main.83fdc16a.chunk.css"
+  },
+  {
+    "revision": "960ca7ae08cb297bef3af1fe87fb271f",
+    "url": "/repo/index.html"
+  }
+];
